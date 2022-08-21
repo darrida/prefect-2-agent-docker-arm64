f332a6734b01b9887210701681e56d339b000ee6
Ben Hammond <darrida.py@gmail.com>
2022-08-21 15:16:16 -0500

    Updated to work with python util file

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN apt-get update && apt-get upgrade -y
 RUN curl -fsSL https://get.docker.com | sh
 
 ADD ./startup.sh startup.sh
+ADD ./work_queues_list.py work_queues_list.py
 RUN chmod +x startup.sh
 ENTRYPOINT ["bash", "startup.sh"]
--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 prefect_key=`cat /run/secrets/prefect-key`
 
 python queues_list.py > queues.txt
-prefect_queues=`cat queues.txt`
+prefect_work_queues=`cat queues.txt`
 
 export PREFECT_API_URL="$PREFECT_API_BASE_URL/api/accounts/$PREFECT_ACCOUNT_ID/workspaces/$PREFECT_WORKSPACE_ID"
 export PREFECT_API_KEY="$prefect_key"
 
 echo "PREFECT_API_URL: $PREFECT_API_URL"
 echo "PREFECT_PROFILE: $PREFECT_PROFILE"
-echo "PREFECT WORK QUEUES: $prefect_queues"
+echo "PREFECT WORK QUEUES: $prefect_work_queues"
 
-prefect agent start $prefect_queues
+prefect agent start $prefect_work_queues
