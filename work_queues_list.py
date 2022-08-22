fb3bc07c4276acf8dd5e87fd07e7b83fd62cdffe
darrida <darrida.py@gmail.com>
2022-08-21 21:32:08 -0500

    Additional comments in python script to explain intention.

--- work_queues_list.py
+++ work_queues_list.py
@@ -1,15 +1,13 @@
 import os
 
 
+# Convert "name1,name2,name,3" >>> "-q name1 -q name2 -q name3"
 work_queues_str = os.environ['PREFECT_WORK_QUEUES']
-
-
 work_queues_str = work_queues_str.replace(" ", "")
 work_queues_l = work_queues_str.split(",")
-
-
 queues_formatted = [f"-q {x}" for x in work_queues_l]
 queues_formatted = ' '.join(queues_formatted)
 
 
+# Print so that startup.sh can grab output
 print(queues_formatted)
